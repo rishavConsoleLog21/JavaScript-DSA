2621f9cdc82130ca75f628f4f95199a626e3b7b0
rishavcool21 <kumarrishavcool21@gmail.com>
2023-09-26 00:28:46 +0530

    change the prototype to new object

--- OOPs/13.Change the Prototype to a New Object/Q&A/jsfile.js
+++ OOPs/13.Change the Prototype to a New Object/Q&A/jsfile.js
@@ -1,9 +1,15 @@
 // Add the property numLegs and the two methods eat() and describe() to the prototype of Dog by setting the prototype to a new object.
 function Dog(name) {
-    this.name = name;
-  }
-  
-  Dog.prototype = {
-    // Only change code below this line
-  
-  };
+  this.name = name;
+}
+
+Dog.prototype = {
+  // Only change code below this line
+  numLegs: 4,
+  eat: function () {
+    console.log("WOW WOW WOW");
+  },
+  describe: function () {
+    console.log("My name is" + this.name);
+  },
+};
