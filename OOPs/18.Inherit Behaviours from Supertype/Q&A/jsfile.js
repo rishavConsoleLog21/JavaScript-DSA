c1e4fce3be4d76e816697e2f4c1508fa2be94e53
rishavcool21 <kumarrishavcool21@gmail.com>
2023-09-27 07:47:03 +0530

    Added 4 more

---
+++ OOPs/18.Inherit Behaviours from Supertype/Q&A/jsfile.js
@@ -0,0 +1,14 @@
+// Use Object.create to make two instances of Animal named duck and beagle.
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+// Only change code below this line
+
+let duck = Object.create(Animal.prototype);
+let beagle= Object.create(Animal.prototype);
